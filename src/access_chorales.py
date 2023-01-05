14ad2aec666854afd20fadb246a80f5c161591a8
Philipp Temmel <philipp@temmel.me>
2023-01-05 22:43:53 +0100

    how to access music21 bach chorales

---
+++ src/access_chorales.py
@@ -0,0 +1,27 @@
+from music21 import corpus
+
+
+if __name__ == '__main__':
+
+    # for score in corpus.chorales.Iterator():
+    #     # score.plot('pianoroll', figureSize=(10, 3))
+    #     # score.measures(1, 3).plot('pianoroll', figureSize=(10, 3))
+    #     score.measures(1, 3).plot('histogram', 'duration', figureSize=(10, 3))
+    #     pass
+
+    # chorales = corpus.search('bach', fileExtensions='xml')
+    # bwv1 = chorales[0].parse()
+    # bwv1.measures(0, 3).show()
+
+    allBach = corpus.search('bach')
+
+    x = allBach[0]
+    p = x.parse()
+
+    partStream = p.parts.stream()
+
+    for n in p.flat.notes:
+        print("Note: %s%d %0.1f" % (n.pitch.name, n.pitch.octave, n.duration.quarterLength))
+
+
+    pass
