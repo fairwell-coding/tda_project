8a7f2808d59345a185eca6140385fcfce37c8676
Philipp Temmel <philipp@temmel.me>
2023-01-14 23:24:52 +0100

    intermediate state

--- src/main.py
+++ src/main.py
@@ -5,11 +5,19 @@ if __name__ == '__main__':
     choral = chorales[0]
     score = choral.parse()
 
-    partStream = score.parts.stream()
+    # partStream = score.parts.stream()
 
-    #score.measures(0, 2).plot('pianoroll', figureSize=(10, 3))
+    # score.measures(1, 31).plot('pianoroll', figureSize=(10, 3))
 
+    note_based_representation = []
     for n in score.flat.notes:
-        # print("Note: %s%d %0.1f" % (n.pitch.name, n.pitch.octave, n.duration.quarterLength))
-        print(f"time: {float(n.duration.quarterLength):.2f}, note: {n.pitch.nameWithOctave}:{n.pitch.midi}, velocity: {n.pitch.frequency:.2f}, note-on: {float(n.offset):.2f}")
+        #print(f"time: {float(n.duration.quarterLength):.2f}, note: {n.pitch.nameWithOctave}:{n.pitch.midi}, velocity: {n.pitch.frequency:.2f}, note-on: {float(n.offset):.2f}")
+        note_based_representation.append([float(n.duration.quarterLength), n.pitch.midi, float(n.offset)])
 
+    data = np.array(note_based_representation)
+    # data = data[:, [True, True, False, True]]
+
+    rips = Rips()
+    diagrams = rips.fit_transform(data)
+    rips.plot(diagrams)
+    plt.show()
